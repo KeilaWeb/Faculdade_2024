3895a89ed93beb42e6bd320a5bc7149be17e93d9
KEILA DAMASCENO BARRETO <keila_barreto@estudante.sesisenai.org.br>
2024-03-14 20:11:33 -0300

    subindo atividades

---
+++ Desenvolvimento Web/API/Aula02/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const userRouter = require("./router/userRouter.js");
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1> Hello World</h1>');
+});
+
+app.use("/api", userRouter);
+
+app.listen(PORT, () => {
+    console.log("Servidor online");
+})
